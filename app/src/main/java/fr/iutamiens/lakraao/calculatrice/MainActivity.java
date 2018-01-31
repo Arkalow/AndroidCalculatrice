33212a997b9035f53a749f4f2c2f9c5cde5a906e
Lakraa Omer <omer.lakraa@etud.u-picardie.fr>
2018-01-31 16:29:52 +0100

    Ajout le calculatrice

--- app/src/main/java/fr/iutamiens/lakraao/calculatrice/MainActivity.java
+++ app/src/main/java/fr/iutamiens/lakraao/calculatrice/MainActivity.java
@@ -3,18 +3,111 @@ package fr.iutamiens.lakraao.calculatrice;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView num_1;
+    private TextView num_2;
+
+    private Button bt_plus;
+    private Button bt_moins;
+    private Button bt_div;
+    private Button bt_fois;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        resultat();
+
+        //Bouton
+        bt_plus = findViewById(R.id.bt_plus);
+        bt_moins = findViewById(R.id.bt_moins);
+        bt_div = findViewById(R.id.bt_div);
+        bt_fois = findViewById(R.id.bt_fois);
+
+        num_1 = findViewById(R.id.txt_1);
+        num_2 = findViewById(R.id.txt_2);
+
+        //Addition
+        bt_plus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    Double res = 0.0;
+                    res = Double.parseDouble(num_1.getText().toString()) + Double.parseDouble(num_2.getText().toString());
+                    resultat(res.toString(), "+");
+                    Log.d("TAG", "Addition");
+                }catch (Exception e){
+                    resultat("impossible", "+");
+                    Log.d("TAG", "Erreur");
+                }
+            }
+        });
+
+        //Soustraction
+        bt_moins.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    Double res = 0.0;
+                    res = Double.parseDouble(num_1.getText().toString()) - Double.parseDouble(num_2.getText().toString());
+                    resultat(res.toString(), "-");
+                    Log.d("TAG", "Soustraction");
+                }catch (Exception e){
+                    resultat("impossible", "-");
+                    Log.d("TAG", "Erreur");
+                }
+
+            }
+        });
+
+        //Division
+        bt_div.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    Double res = 0.0;
+                    res = Double.parseDouble(num_1.getText().toString()) / Double.parseDouble(num_2.getText().toString());
+                    resultat(res.toString(), "/");
+                    Log.d("TAG", "Division");
+                }catch (Exception e){
+                    resultat("impossible", "/");
+                    Log.d("TAG", "Erreur");
+                }
+            }
+        });
+
+        //Multiplication
+        bt_fois.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    Double res = 0.0;
+                    res = Double.parseDouble(num_1.getText().toString()) * Double.parseDouble(num_2.getText().toString());
+                    resultat(res.toString(), "*");
+                    Log.d("TAG", "Multiplication");
+                }catch (Exception e){
+                    resultat("impossible", "*");
+                    Log.d("TAG", "Erreur");
+                }
+            }
+        });
     }
-    private void resultat(){
+
+    private void resultat(String resultat, String operateur){
+        ArrayList<String> donnees = new ArrayList<String>();
+        donnees.add(num_1.getText().toString());
+        donnees.add(num_2.getText().toString());
+        donnees.add(operateur);
+        donnees.add(resultat);
         Intent intent = new Intent(this, ResultatActivity.class);
-        intent.putExtra("value", "test");
+        intent.putExtra("array_list", donnees);
         startActivity(intent);
     }
 }
--- app/src/main/java/fr/iutamiens/lakraao/calculatrice/ResultatActivity.java
+++ app/src/main/java/fr/iutamiens/lakraao/calculatrice/ResultatActivity.java
@@ -7,20 +7,25 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 public class ResultatActivity extends AppCompatActivity {
 
     private TextView text;
-
+    private String msg;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resultat);
 
         text= findViewById(R.id.textView);
-        Bundle b = getIntent().getExtras();
 
-        if (b != null) {
-            text.setText(b.get("value").toString());
+        Bundle b = getIntent().getExtras();
+        if (null != b) {
+            ArrayList<String> donnees = b.getStringArrayList("array_list");
+            msg = donnees.get(0) + " " + donnees.get(2) + " " + donnees.get(1) + " = " + donnees.get(3);
+            text.setText(msg);
         }
     }
     @Override
@@ -32,7 +37,7 @@ public class ResultatActivity extends AppCompatActivity {
         if(item.getItemId() == R.id.share){
             Intent sendIntent = new Intent();
             sendIntent.setAction(Intent.ACTION_SEND);
-            sendIntent.putExtra(Intent.EXTRA_TEXT, "Je partage des trucs");
+            sendIntent.putExtra(Intent.EXTRA_TEXT, msg);
             sendIntent.setType("text/plain");
             startActivity(sendIntent);
         }
