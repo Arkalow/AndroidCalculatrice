1e5455318c5dea8d2237ea7b99e1496cc2c8f12c
Lakraa Omer <omer.lakraa@etud.u-picardie.fr>
2018-01-31 15:13:53 +0100

    Partage dans activite resultat

--- app/src/main/java/fr/iutamiens/lakraao/calculatrice/ResultatActivity.java
+++ app/src/main/java/fr/iutamiens/lakraao/calculatrice/ResultatActivity.java
@@ -1,7 +1,10 @@
 package fr.iutamiens.lakraao.calculatrice;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class ResultatActivity extends AppCompatActivity {
 
@@ -9,5 +12,20 @@ public class ResultatActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resultat);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == R.id.share){
+            Intent sendIntent = new Intent();
+            sendIntent.setAction(Intent.ACTION_SEND);
+            sendIntent.putExtra(Intent.EXTRA_TEXT, "Je partage des trucs");
+            sendIntent.setType("text/plain");
+            startActivity(sendIntent);
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
